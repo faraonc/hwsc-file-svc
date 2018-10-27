8999a58a54e81fe81c011646b4f34f82fb22af0e
actang <act.alextang@gmail.com>
2018-10-27 15:11:12 -0700

    separated client.py from main.py

---
+++ client.py
@@ -0,0 +1,18 @@
+import os
+import service.hwsc_file_transaction_svc as hwsc_file_transaction_svc
+
+if __name__ == '__main__':
+    client = hwsc_file_transaction_svc.FileTransactionClient('localhost:50051')
+
+    # download data
+    download_request = ''
+    if os.path.exists(download_request):
+        os.remove(download_request)
+    client.download('downLoad_data',download_request)
+
+    # upload data
+    upload_request = ''
+    client.upload(upload_request)
+
+    os.system(f' sha1sum{download_request}')
+    os.system(f' sha1sum{upload_request}')
--- main.py
+++ main.py
@@ -2,25 +2,3 @@ import service.hwsc_file_transaction_svc as hwsc_file_transaction_svc
 
 if __name__ == '__main__':
     hwsc_file_transaction_svc.FileTransactionService().start(50051)
-
-
-#------------------------------------------------------
-# This should be in a separate client.py file ???
-import os
-import service.hwsc_file_transaction_svc as hwsc_file_transaction_svc
-
-if __name__ == '__main__':
-    client = hwsc_file_transaction_svc.FileTransactionClient('localhost:50051')
-
-    # download data
-    download_request = ''
-    if os.path.exists(download_request):
-        os.remove(download_request)
-    client.download('downLoad_data',download_request)
-
-    # upload data
-    upload_request = ''
-    client.upload(upload_request)
-
-    os.system(f' sha1sum{download_request}')
-    os.system(f' sha1sum{upload_request}')
