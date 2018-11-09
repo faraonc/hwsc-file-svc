e44c882eb85a0dde12ee3364102059f244836719
Mandy <dinhbl@uw.edu>
2018-11-09 13:19:39 -0800

    [WIP]uploadFile

--- hwsc_file_transaction_svc.py
+++ hwsc_file_transaction_svc.py
@@ -9,18 +9,21 @@ import hwsc_file_transaction_svc_pb2_grpc
 
 CHUNK_SIZE = 1024 * 1024
 
-def download_chunk(reqFile):
-    with open(reqFile, 'rb')as f:
+
+def download_chunk(file):
+    with open(file, 'rb')as f:
         while True:
-            chunk = f.read(CHUNK_SIZE);
+            chunk = f.read(CHUNK_SIZE)
             if len(chunk) == 0:
                 return
             yield hwsc_file_transaction_svc_pb2.chunk(buffer=chunk)
 
+
 def save_chunks_to_file(chunks, filename):
-    with open(filename,'wb') as f:
+    with open(filename, 'wb') as f:
         for chunk in chunks:
             f.write(chunk.buffer)
+
 
 class FileTransactionService(hwsc_file_transaction_svc_pb2_grpc.FileTransactionServiceServicer):
     def __init__(self):
@@ -32,17 +35,27 @@ class FileTransactionService(hwsc_file_transaction_svc_pb2_grpc.FileTransactionS
             def GetStatus(self, request, context):
                 print("Get Status")
 
-            def DownloadZipFiles(self,downloadRequest,context):
-                if downloadRequest.name:
+            def DownloadZipFiles(self, request_iterator, context):
+                if request_iterator.name:
                     return download_chunk(self.tmp_file_name)
 
             def UploadFile(self, request_iterator, context):
                 print("[INFO] Requesting UploadFile service")
-                #save_chunks_to_file(request_iterator,'dummy_img.jpg')
-                save_chunks_to_file(request_iterator, context.fileName)
-                #return hwsc_file_transaction_svc_pb2.FileTransactionResponse(message='Hello', url='world', length=64)
+                save_chunks_to_file(request_iterator, request_iterator.fileName)
+
+                if not request_iterator.buffer.is_valid or request_iterator.fileName.is_valid:
+                    message = 'Upload Error!'
+                    context.set_details(message)
+                    context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+                    return hwsc_file_transaction_svc_pb2.FileTransactionResponse()
+
+                status = hwsc_file_transaction_svc_pb2.FileTransactionResponse()
+                status.code = grpc.StatusCode.OK
+                assert status.HasField("code")
+
                 return hwsc_file_transaction_svc_pb2.FileTransactionResponse(
-                    message=context.fileName + 'has been uploaded!',
+                    message='OK',
+                    status=status.code,
                     url='url: /res' + context.fileName,
                     length=64)
 
@@ -51,16 +64,12 @@ class FileTransactionService(hwsc_file_transaction_svc_pb2_grpc.FileTransactionS
         hwsc_file_transaction_svc_pb2_grpc.add_FileTransactionServiceServicer_to_server(Servicer(), self.server)
 
     def start(self, port):
-        print("1")
-
         self.server.add_insecure_port(f'[::]:{port}')
-        print("2")
-
         self.server.start()
-        print("3")
+        print("Server is running...")
 
         try:
             while True:
-                time.sleep(60*60*24)
+                time.sleep(60 * 60 * 24)
         except KeyboardInterrupt:
             self.server.stop(0)
