77836c96bcdcac1ba71f050c9e9fb5f40bfd8d14
Lisa Kim <lisakeem@outlook.com>
2018-12-01 21:11:42 -0800

    Update package faraonc to hwsc-org

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@
 # This should be changed to different github link that points to our particular project
 # #egg=[] should be specified in order to run.
 #Example: pipenv install -e git+https://github.com/requests/requests.git#egg=requests
-#pipenv install -e git+https://github.com/faraonc/hwsc-api-blocks@master#egg=hwsc_transaction_svc_pb
+#pipenv install -e git+https://github.com/hwsc-org/hwsc-api-blocks@master#egg=hwsc_transaction_svc_pb
 #git sparse checkout
 #how to make a repo pipenv
 
