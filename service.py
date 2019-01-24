ebb331aa08aaeeb63e83e67f6c855f279bf5eebb
Yeseul An <yeseul90@uw.edu>
2019-01-23 20:21:48 -0800

    Add docstring and recreate proto with comments

--- service.py
+++ service.py
@@ -7,38 +7,46 @@ import hwsc_file_transaction_svc_pb2
 import hwsc_file_transaction_svc_pb2_grpc
 
 class FileTransactionService(hwsc_file_transaction_svc_pb2_grpc.FileTransactionServiceServicer):
+     """A FileTransactionService class contains servicer and corresponding functionalites."""
      def __init__(self):
 
          class Servicer(hwsc_file_transaction_svc_pb2_grpc.FileTransactionServiceServicer):
+             """A servicer class contains functionalities for file service."""
              def __init__(self):
                  pass
 
+             #TODO
              def GetStatus(self, request, context):
+                 """Get and return status to the user."""
                  print("Get Status")
 
+             #TODO
              def DownloadZippedFiles(self, request_iterator, context):
+                 """Download zipped files from azrue blob storage."""
                  if request_iterator.name:
                      return utility.download_chunk(self.tmp_file_name)
 
              def UploadFile(self, request_iterator, context):
+                 """Upload a file to the azure blob storage."""
                  print("[INFO] Requesting UploadFile service")
                  #TODO
                  #May implement try-catch block
 
                  d = utility.get_property(request_iterator)
 
-                 count = utility.count_folders(d['uuid'])
-                 is_uuid_valid = utility.verify_uuid(d['uuid'])
+                 type = utility.get_file_type(d["f_name"])
+                 is_uuid_valid = utility.verify_uuid(d["uuid"])
 
                  if is_uuid_valid:
-                     get_url = utility.upload_file_to_azure(d['stream'], count, d['uuid'], d['f_name'])
+                     has_folder = utility.find_folder(d["uuid"], type)
+                     get_url = utility.upload_file_to_azure(d["stream"], has_folder, d["uuid"], d["f_name"])
 
                      #TODO
                      #write a function to validate the url, like a regex check
                      if get_url != "":
                         return hwsc_file_transaction_svc_pb2.FileTransactionResponse(
                             code=grpc.StatusCode.OK.value[0],
-                            message='success uploadfile',
+                            message="success uploadfile",
                             url=get_url
                         )
 
@@ -55,6 +63,7 @@ class FileTransactionService(hwsc_file_transaction_svc_pb2_grpc.FileTransactionS
                      )
 
              def CreateUserFolder(self, request, context):
+                """Create user folder in the azure blob storage."""
                  #TODO
                  #May implement try-catch block
                 print("[INFO] Requesting CreateUuidFolder service")
--- utility.py
+++ utility.py
@@ -15,7 +15,8 @@ block_blob_service = BlockBlobService(account_name=config.CONFIG["storage"],
                                           account_key=config.CONFIG["storage_key"])
 
 def download_chunk(file):
-     with open(file, 'rb')as f:
+     """Download a file in chunks."""
+     with open(file, "rb")as f:
          while True:
              chunk = f.read(CHUNK_SIZE)
              if len(chunk) == 0:
@@ -23,6 +24,7 @@ def download_chunk(file):
              yield hwsc_file_transaction_svc_pb2.chunk(buffer=chunk)
 
 def get_file_type(file_name):
+    """Check and return correct type of the file passed in."""
     image_exts_dict = {"jpg": True, "jpeg": True, "png": True, "bmp": True, "tif": True, "gif": True, "tiff": True}
     audio_exts_dict = {"wav": True, "wma": True, "ogg": True, "m4a": True, "mp3": True}
     video_exts_dict = {"flv": True, "wmv": True, "mov": True, "avi": True, "mp4": True}
@@ -40,13 +42,13 @@ def get_file_type(file_name):
 
     return file_type
 
-def upload_file_to_azure(stream, count, uuid, file_name):
+def upload_file_to_azure(stream, has_folder, uuid, file_name):
+    """Upload file to azure blob storage."""
     #TODO
     #ADD try-catch block
 
     # Checking whether the blobs that associated with uuid exists
-    if count == 4:
-        print(count)
+    if has_folder:
         container_type = get_file_type(file_name)
         container_name = uuid + '-' + container_type
 
@@ -59,17 +61,18 @@ def upload_file_to_azure(stream, count, uuid, file_name):
 
         stream.seek(0)
 
-        if block_blob_service.exists(container_name):
-            block_blob_service.create_blob_from_stream(container_name, file_name, stream)
-            print("[DEBUG]Uploading to folder with the file name:", file_name)
+        block_blob_service.create_blob_from_stream(container_name, file_name, stream)
+        print("[DEBUG]Uploading to folder with the file name:", file_name)
 
-            url_upload = block_blob_service.make_blob_url(container_name, file_name)
-            print(url_upload)
-            return url_upload
+        url_upload = block_blob_service.make_blob_url(container_name, file_name)
+        print(url_upload)
+        return url_upload
+
     else:
         return ""
 
 def create_uuid_container_in_azure(count, uuid):
+    """Create uuid folder in the azure blob storage."""
     # TODO
     # ADD try-catch block
     if count == 0:
@@ -89,16 +92,26 @@ def create_uuid_container_in_azure(count, uuid):
         print("[Utility]Successful to create folders.")
         return False
 
-def verify_uuid(uuid):
+def verify_uuid(id):
+    """Verify uuid and checks it matches correct format."""
     uuid_regex = re.compile(r'^[a-zA-Z0-9]{26}$')
-    is_uuid_valid = uuid_regex.match(uuid)
 
-    if is_uuid_valid:
+    if uuid_regex.match(id):
+        return True
+    else:
+        return False
+
+def find_folder(uuid, f_name):
+    """Find folder in the azure blob storage."""
+    container_name = uuid + '-' + f_name
+
+    if block_blob_service.exists(container_name):
         return True
     else:
         return False
 
 def count_folders(uuid):
+    """Count folders in azure blob storage."""
     list_generator = block_blob_service.list_containers(uuid)
 
     folder_count = 0
@@ -108,18 +121,19 @@ def count_folders(uuid):
     return folder_count
 
 def get_property(request_iterator):
+    """Store and return properties of filename, uuid, and buffer."""
     stream = io.BytesIO()
     d = dict()
 
     for property in request_iterator:
         if len(property.uuid) > 1:
-            d['uuid'] = property.uuid
+            d["uuid"] = property.uuid
         if len(property.file_name) > 1:
-            d['f_name'] = property.file_name
+            d["f_name"] = property.file_name
         if len(property.buffer) > 1:
             stream.write(property.buffer)
         else:
             pass
 
-        d['stream'] = stream
+        d["stream"] = stream
     return d
