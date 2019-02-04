a0f6e8998164d9fa50c9cc73b348b21cdc802e05
Mandy <dinhbl@uw.edu>
2019-02-03 16:29:56 -0800

    fix code reivew errors

--- service.py
+++ service.py
@@ -1,84 +1,94 @@
 from concurrent import futures
 import grpc
-import time
 import utility
-import io
 import hwsc_file_transaction_svc_pb2
 import hwsc_file_transaction_svc_pb2_grpc
 from enum import Enum
 
+
+state = 0
+
+
 class State(Enum):
+    """A current state class of file transaction service"""
     AVAILABLE = 0
     UNAVAILABLE = 1
 
+
 class FileTransactionService(hwsc_file_transaction_svc_pb2_grpc.FileTransactionServiceServicer):
-     """A FileTransactionService class contains servicer and corresponding functionalites."""
-     def __init__(self):
+    """A FileTransactionService class contains servicer and corresponding functionalites."""
 
-         class Servicer(hwsc_file_transaction_svc_pb2_grpc.FileTransactionServiceServicer):
-             """A servicer class contains functionalities for file service."""
-             def __init__(self):
-                 pass
+    def __init__(self):
 
-             def GetStatus(self, request, context):
-                 print("[INFO] Requesting GetStatus service")
+        class Servicer(hwsc_file_transaction_svc_pb2_grpc.FileTransactionServiceServicer):
+            """A servicer class contains functionalities for file service."""
 
-                 if(grpc.StatusCode.OK.value[0]!= State.AVAILABLE.value):
-                     context.set_code = grpc.StatusCode.UNAVAILABLE.value[0]
-                     context.set_details = grpc.StatusCode.UNAVAILABLE.value[1]
+            def __init__(self):
+                pass
 
-                 else:
-                     context.set_code = grpc.StatusCode.OK.value[0]
-                     context.set_details = grpc.StatusCode.OK.value[1]
+            def GetStatus(self, request, context):
+                """Return the status of the service"""
+                print("[INFO] Requesting GetStatus service")
+                global state
+                state = State.AVAILABLE.value
+                available = grpc.StatusCode.OK.value[0]
 
-                 return hwsc_file_transaction_svc_pb2.FileTransactionResponse(
-                     code=context.set_code,
-                     message=context.set_details
-                 )
+                if state != available:
+                    context.set_code = grpc.StatusCode.UNAVAILABLE.value[0]
+                    context.set_details = grpc.StatusCode.UNAVAILABLE.name
 
-             """Upload a file to the azure blob storage."""
-             def UploadFile(self, request_iterator, context):
-                 print("[INFO] Requesting UploadFile service")
+                else:
+                    context.set_code = grpc.StatusCode.OK.value[0]
+                    context.set_details = grpc.StatusCode.OK.name
 
-                 d = utility.get_property(request_iterator)
+                return hwsc_file_transaction_svc_pb2.FileTransactionResponse(
+                    code=context.set_code,
+                    message=context.set_details
+                    )
 
-                 type = utility.get_file_type(d["f_name"])
-                 print("print file name")
-                 print(type)
+            def UploadFile(self, request_iterator, context):
+                """Upload a file to the azure blob storage."""
+                print("[INFO] Requesting UploadFile service")
 
-                 is_uuid_valid = utility.verify_uuid(d["uuid"])
-                 print("print is valid uuid")
-                 print(is_uuid_valid)
+                d = utility.get_property(request_iterator)
 
-                 if is_uuid_valid:
-                     has_folder = utility.find_folder(d["uuid"], type)
+                type = utility.get_file_type(d["f_name"])
+                print("print file name")
+                print(type)
 
-                     print("print has_folder")
-                     print(has_folder)
+                is_uuid_valid = utility.verify_uuid(d["uuid"])
+                print("print is valid uuid")
+                print(is_uuid_valid)
 
-                     get_url = utility.upload_file_to_azure(d["stream"], has_folder, d["uuid"], d["f_name"])
+                if is_uuid_valid:
+                    has_folder = utility.find_folder(d["uuid"], type)
 
-                     if get_url != "":
+                    print("print has_folder")
+                    print(has_folder)
+
+                    get_url = utility.upload_file_to_azure(d["stream"], has_folder, d["uuid"], d["f_name"])
+
+                    if get_url != "":
                         return hwsc_file_transaction_svc_pb2.FileTransactionResponse(
                             code=grpc.StatusCode.OK.value[0],
                             message="success uploadfile",
                             url=get_url
-                        )
+                            )
 
-                     else:
+                    else:
                         return hwsc_file_transaction_svc_pb2.FileTransactionResponse(
                             code=grpc.StatusCode.UNKNOWN.value[0],
                             message='fail uploadfile',
+                            )
+
+                else:
+                    return hwsc_file_transaction_svc_pb2.FileTransactionResponse(
+                        code=grpc.StatusCode.UNKNOWN.value[0],
+                        message="invalid uuid"
                         )
 
-                 else:
-                     return hwsc_file_transaction_svc_pb2.FileTransactionResponse(
-                         code=grpc.StatusCode.UNKNOWN.value[0],
-                         message="invalid uuid"
-                     )
-
-             """Create user folder in the azure blob storage."""
-             def CreateUserFolder(self, request, context):
+            def CreateUserFolder(self, request, context):
+                """Create user folder in the azure blob storage."""
                 print("[INFO] Requesting CreateUuidFolder service")
 
                 is_uuid_valid = utility.verify_uuid(request.uuid)
@@ -93,38 +103,40 @@ class FileTransactionService(hwsc_file_transaction_svc_pb2_grpc.FileTransactionS
                         return hwsc_file_transaction_svc_pb2.FileTransactionResponse(
                             code=grpc.StatusCode.OK.value[0],
                             message="success"
-                        )
+                            )
                     else:
                         return hwsc_file_transaction_svc_pb2.FileTransactionResponse(
                             code=grpc.StatusCode.UNKNOWN.value[0],
                             message="user folder already exist"
-                        )
+                            )
                 else:
                     return hwsc_file_transaction_svc_pb2.FileTransactionResponse(
                         code=grpc.StatusCode.UNKNOWN.value[0],
                         message="invalid uuid"
-                    )
+                        )
 
-             #TODO
-             def DownloadZippedFiles(self, request_iterator, context):
-                 """Download zipped files from azrue blob storage."""
-                 if request_iterator.name:
-                     return utility.download_chunk(self.tmp_file_name)
+            # TODO
+            """Download zipped files from azrue blob storage."""
 
-             #TODO
-             self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+            def DownloadZippedFiles(self, request_iterator, context):
 
-         hwsc_file_transaction_svc_pb2_grpc.add_FileTransactionServiceServicer_to_server(Servicer(), self.server)
+                if request_iterator.name:
+                    return utility.download_chunk(self.tmp_file_name)
 
-     def start(self, port):
-         self.server.add_insecure_port(f'[::]:{port}')
-         self.server.start()
-         print("[INFO] hwsc-file-transaction-svc initializing...")
-         print("[INFO] hwsc-file-transaction started at:")
-         print(port)
+            # TODO
+            self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
 
-         try:
-           while True:
+        hwsc_file_transaction_svc_pb2_grpc.add_FileTransactionServiceServicer_to_server(Servicer(), self.server)
+
+    def start(self, port):
+        self.server.add_insecure_port(f'[::]:{port}')
+        self.server.start()
+        print("[INFO] hwsc-file-transaction-svc initializing...")
+        print("[INFO] hwsc-file-transaction started at:")
+        print(port)
+
+        try:
+            while True:
                 pass
-         except KeyboardInterrupt:
-                  self.server.stop(0)
+        except KeyboardInterrupt:
+            self.server.stop(0)
