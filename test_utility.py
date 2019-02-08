bb783301be607a6c6c1275ad7525ab1f56c8d941
dinhl <35046106+dinhl@users.noreply.github.com>
2019-02-07 21:03:02 -0800

    Implement GetStatus without synchronization (#34)
    * add unit test for test_utility.py
    
    * add feature getStatus
    
    * fix code reivew errors
    
    * change global variable
    
    * add state to constructor
    
    * place state outside constructor
    
    * fix state error

--- test_utility.py
+++ test_utility.py
@@ -1,22 +1,47 @@
 import pytest
 import utility
 
-# testing with already existing uuid
-def test_valid_uuid():
-    with pytest.raises(Exception) as e_info:
-        utility.create_uuid_container_in_azure("1234abcd5454efef8842ll3fsc")
+def test_get_file_type():
+    assert utility.get_file_type("cat.jpg") == "images"
+    assert utility.get_file_type("cat.jpeg") == "images"
+    assert utility.get_file_type("cat.png") == "images"
+    assert utility.get_file_type("cat.bmp") == "images"
+    assert utility.get_file_type("cat.tif") == "images"
+    assert utility.get_file_type("cat.gif") == "images"
+    assert utility.get_file_type("cat.tiff") == "images"
 
-# testing with shorter length uuid
-def test_valid_uuid1():
-    with pytest.raises(Exception) as e_info:
-        utility.create_uuid_container_in_azure("1234abcd5454efef8842ll3fs")
+    assert utility.get_file_type("cat.wav") == "audios"
+    assert utility.get_file_type("cat.wma") == "audios"
+    assert utility.get_file_type("cat.ogg") == "audios"
+    assert utility.get_file_type("cat.m4a") == "audios"
+    assert utility.get_file_type("cat.mp3") == "audios"
 
-# testing with special case in uuid
-def test_valid_uuid2():
-    with pytest.raises(Exception) as e_info:
-        utility.create_uuid_container_in_azure("1234abcd5454efef8842ll3!ss")
+    assert utility.get_file_type("cat.flv") == "videos"
+    assert utility.get_file_type("cat.wmv") == "videos"
+    assert utility.get_file_type("cat.mov") == "videos"
+    assert utility.get_file_type("cat.avi") == "videos"
+    assert utility.get_file_type("cat.mp4") == "videos"
 
-# should be not passed. but how?
-def test_valid_uuid4():
-    with pytest.raises(Exception) as e_info:
-        utility.create_uuid_container_in_azure("1234abcd5454efef8842llaaaa")
+    assert utility.get_file_type("cat.dat") == "files"
+    assert utility.get_file_type("cat.xml") == "files"
+    assert utility.get_file_type("cat.csv") == "files"
+    assert utility.get_file_type("cat.py") == "files"
+    assert utility.get_file_type("cat.html") == "files"
+
+def test_create_uuid_container_in_azure():
+     assert utility.create_uuid_container_in_azure(0, "1234abcd5454efef8842ll3fsc") == True
+     assert utility.create_uuid_container_in_azure(4, "1234abcd5454efef8842ll3fsc") == False
+     assert utility.create_uuid_container_in_azure(-4, "1234abcd5454efef8842ll3fsc") == False
+
+def test_verify_uuid():
+    # testing with correct uuid
+    assert utility.verify_uuid("1234abcd5454efef8842ll3fsc") == True
+
+    # testing with shorter length uuid
+    assert utility.verify_uuid("1234abcd5454efef8842ll3fs") == False
+
+    # testing with longer length uuid
+    assert utility.verify_uuid("1234abcd5454efef8842llaaaab") == False
+
+    # testing with special case in uuid
+    assert utility.verify_uuid("1234abcd5454efef8842ll3!ss") == False
