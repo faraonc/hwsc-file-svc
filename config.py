6e30763859295cd2b522a38b178f394e61589179
Yeseul An <yeseul90@uw.edu>
2019-02-10 23:24:32 -0800

    Update environment variable

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 CONFIG = {
-    "address": os.environ.get("hosts_grpc_address"),
-    "port": os.environ.get("hosts_grpc_port"),
-    "hosts_grpc_network": os.environ.get("hosts_grpc_network"),
+    "address": os.environ.get("hosts_file_address"),
+    "port": os.environ.get("hosts_file_port"),
+    "hosts_grpc_network": os.environ.get("hosts_file_network"),
     "storage": os.environ.get("hosts_azure_storage"),
     "storage_key": os.environ.get("hosts_azure_key")
 }
