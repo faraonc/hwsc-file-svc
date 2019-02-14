a00e59559245fecd8e93607dd3bc445e4e343d0a
CJ Faraon <faraonc@outlook.com>
2019-02-13 22:47:12 -0800

    Fix imports

--- generate_dep.sh
+++ generate_dep.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/proto/hwsc-file-transaction-svc.proto
-wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/proto/hwsc_file_transaction_svc_pb2.py
-wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/proto/hwsc_file_transaction_svc_pb2_grpc.py
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/hwsc-file-transaction-svc.proto
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/hwsc_file_transaction_svc_pb2.py
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/hwsc_file_transaction_svc_pb2_grpc.py
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/lib/authority_pb2.py -P lib/
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/lib/authority_pb2_grpc.py lib/
 pipenv install
