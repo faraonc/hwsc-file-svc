c65392b0a2ea39605f7209377d8610dff984b04c
CJ Faraon <faraonc@outlook.com>
2019-03-09 12:15:02 -0800

    Update dependencies

--- generate_dep.sh
+++ generate_dep.sh
@@ -4,15 +4,15 @@
 rm hwsc-file-transaction-svc.proto
 rm hwsc_file_transaction_svc_pb2.py
 rm hwsc_file_transaction_svc_pb2_grpc.py
-rm lib/authority.proto
-rm lib/authority_pb2.py
-rm lib/authority_pb2_grpc.py
+rm int/lib/authority.proto
+rm int/lib/authority_pb2.py
+rm int/lib/authority_pb2_grpc.py
 
 # pull proto and protocol buffers
 wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/hwsc-file-transaction-svc.proto
 wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/hwsc_file_transaction_svc_pb2.py
 wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/hwsc_file_transaction_svc_pb2_grpc.py
-wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/lib/authority.proto -P lib/
-wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/lib/authority_pb2.py -P lib/
-wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/lib/authority_pb2_grpc.py -P lib/
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/int/lib/authority.proto -P int/lib/
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/int/lib/authority_pb2.py -P int/lib/
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/int/lib/authority_pb2_grpc.py -P int/lib/
 pipenv install --dev
