974104f0e87b7a5d03803a790293b8e467d5afa4
faraonc <faraonc@outlook.com>
2019-03-10 03:30:26 -0700

    Use new protobuf folder for api-blocks

--- generate_dep.sh
+++ generate_dep.sh
@@ -4,15 +4,15 @@
 rm hwsc-file-transaction-svc.proto
 rm hwsc_file_transaction_svc_pb2.py
 rm hwsc_file_transaction_svc_pb2_grpc.py
-rm int/lib/authority.proto
-rm int/lib/authority_pb2.py
-rm int/lib/authority_pb2_grpc.py
+rm protobuf/lib/authority.proto
+rm protobuf/lib/authority_pb2.py
+rm protobuf/lib/authority_pb2_grpc.py
 
 # pull proto and protocol buffers
-wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/hwsc-file-transaction-svc.proto
-wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/hwsc_file_transaction_svc_pb2.py
-wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/hwsc_file_transaction_svc_pb2_grpc.py
-wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/int/lib/authority.proto -P int/lib/
-wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/int/lib/authority_pb2.py -P int/lib/
-wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/int/hwsc-file-transaction-svc/file/int/lib/authority_pb2_grpc.py -P int/lib/
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/protobuf/hwsc-file-transaction-svc/file/hwsc-file-transaction-svc.proto
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/protobuf/hwsc-file-transaction-svc/file/hwsc_file_transaction_svc_pb2.py
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/protobuf/hwsc-file-transaction-svc/file/hwsc_file_transaction_svc_pb2_grpc.py
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/protobuf/hwsc-file-transaction-svc/file/protobuf/lib/authority.proto -P protobuf/lib/
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/protobuf/hwsc-file-transaction-svc/file/protobuf/lib/authority_pb2.py -P protobuf/lib/
+wget https://raw.githubusercontent.com/hwsc-org/hwsc-api-blocks/master/protobuf/hwsc-file-transaction-svc/file/protobuf/lib/authority_pb2_grpc.py -P protobuf/lib/
 pipenv install --dev
