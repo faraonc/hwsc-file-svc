eecfd31ecd434e79c786e3de78ec26644b6186ca
Mandy <dinhbl@uw.edu>
2019-04-11 16:56:41 -0700

    Edit function names, variable names

--- jwt.py
+++ jwt.py
@@ -1,45 +1,38 @@
 import base64
 
 
-def element(token):
+def get_element(token):
     result = [x.strip() for x in token.split('.')]
     return result
 
-def header():
-    result = element(token)
+def get_header():
+    result = get_element(token)
     header = result[0]
-
-    #header = "eyJBbGciOjIsIlRva2VuVHlwIjoxfQ";
 
     for x in range(0,(32-len(header))):
         header += "="
 
-    decode_1 = base64.b64decode(header)
-    decode_2 = decode_1.decode()
+    header_value = (base64.b64decode(header)).decode()
+    print("Header is : " + header_value)
 
-    print(decode_2)
-
-def body():
-    # body = "eyJVVUlEIjoiMDFkM3gzd20ybm5yZGZ6cDB0a2Eydnc5ZHgiLCJQZXJtaXNzaW9uIjozLCJFeHBpcmF0aW9uVGltZXN0YW1wIjoxODkzNDU2MDAwfQ";
-    result = element(token)
+def get_body():
+    result = get_element(token)
     body = result[1]
 
     for x in range(0, (116 - len(body))):
         body += "=";
 
-    decode_1 = base64.b64decode(body)
-    decode_2 = decode_1.decode()
+    body_value = (base64.b64decode(body)).decode()
+    print("Body is : " + body_value)
 
-    print(decode_2)
-
-def signature():
-    result = element(token)
+def get_signature():
+    result = get_element(token)
     signature = result[2]
-    print(signature)
+    print("Signature is : " + signature)
 
 if __name__== "__main__":
     token = "eyJBbGciOjIsIlRva2VuVHlwIjoxfQ.eyJVVUlEIjoiMDFkM3gzd20ybm5yZGZ6cDB0a2Eydnc5ZHgiLCJQZXJtaXNzaW9uIjozLCJFeHBpcmF0aW9uVGltZXN0YW1wIjoxODkzNDU2MDAwfQ.8lVhZo_W6KmGI2oi5JNHioDvPq2Yl86v4uae3RfKc-qoKUwHNxFtXO2NFmChsi35__t1uC_SD-Ay_MoateeDNg=="
 
-    header()
-    body()
-    signature()
+    get_header()
+    get_body()
+    get_signature()
