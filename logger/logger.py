66a29d2d12b61a2434fae91e27949c67e6d2201e
Alex Tang <act.alextang@gmail.com>
2019-04-18 00:21:39 -0700

    Implement logger and refactor logging messages

---
+++ logger/logger.py
@@ -0,0 +1,31 @@
+import logging
+from logger.consts import errors
+
+
+logging.addLevelName(50, "FATAL")
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s", datefmt="%y/%m/%d %H:%M:%S")
+
+
+def request_service(service):
+    logging.info("Requesting %s service", service)
+
+
+def debug(*args):
+    logging.debug(" ".join(args))
+
+
+def info(*args):
+    logging.info(" ".join(args))
+
+
+def error(*args):
+    logging.error(" ".join(args))
+
+
+def exception(*args):
+    logging.exception(" ".join(args))
+
+
+def fatal(*args):
+    logging.fatal(" ".join(args))
+
