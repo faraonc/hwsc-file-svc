f7ba032a0a583c8dd1936548d29a828e04f0642b
Mandy <dinhbl@uw.edu>
2019-04-25 20:42:12 -0700

    fix error

--- auth/jwt.py
+++ auth/jwt.py
@@ -92,40 +92,32 @@ def validate_body(body_dict):
 
 
 def validate_permission_with_alg(permission, alg):
-
     if permission == PermissionEnum.ADMIN and alg != AlgEnum.HS512:
         raise ValueError("admin permission not valid")
 
 
 def validate_permission_requirement(req_permission, token_permission):
-
-    if req_permission < token_permission:
+    if req_permission > token_permission:
         raise ValueError("invalid permission requirement")
 
 
 def validate_signature(header_body_token, secret_key, header_dict, signature_token):
-    # signature_hs_512 = hmac.new(b'j2Yzh-VcIm-lYUzBuqt8TVPeUHNYB5MP1gWvz3Bolow=', b'eyJBbGciOjIsIlRva2VuVHlwIjoxfQ.eyJVVUlEIjoiMDFkM3gzd20ybm5yZGZ6cDB0a2Eydnc5ZHgiLCJQZXJtaXNzaW9uIjozLCJFeHBpcmF0aW9uVGltZXN0YW1wIjoxODkzNDU2MDAwfQ', hashlib.sha512).digest()
-    # print(base64.urlsafe_b64encode(signature_hs).decode())
-
-    if header_dict["Alg"] == 1:
+    if header_dict["Alg"] == AlgEnum.HS256:
         signature_rebuild = base64.urlsafe_b64encode(hmac.new(secret_key.encode(),
-                                                         header_body_token.encode(),
-                                                         hashlib.sha256).digest()).decode()
-    elif header_dict["Alg"] == 2:
+                                                              header_body_token.encode(),
+                                                              hashlib.sha256).digest()).decode()
+    elif header_dict["Alg"] == AlgEnum.HS512:
         signature_rebuild = base64.urlsafe_b64encode(hmac.new(secret_key.encode(),
-                                                         header_body_token.encode(),
-                                                         hashlib.sha512).digest()).decode()
+                                                              header_body_token.encode(),
+                                                              hashlib.sha512).digest()).decode()
     else:
         raise ValueError("invalid Alg")
 
     if signature_rebuild != signature_token:
-        raise ValueError("Invalid Signature")
+        raise ValueError("invalid Signature")
 
 
-# if __name__ == "__main__":
 def validate(token_str, secret_key, req_permission):
-    # token_str = "eyJBbGciOjIsIlRva2VuVHlwIjoxfQ.eyJVVUlEIjoiMDFkM3gzd20ybm5yZGZ6cDB0a2Eydnc5ZHgiLCJQZXJtaXNzaW9uIjozLCJFeHBpcmF0aW9uVGltZXN0YW1wIjoxODkzNDU2MDAwfQ.8lVhZo_W6KmGI2oi5JNHioDvPq2Yl86v4uae3RfKc-qoKUwHNxFtXO2NFmChsi35__t1uC_SD-Ay_MoateeDNg=="
-    # secret_key = "j2Yzh-VcIm-lYUzBuqt8TVPeUHNYB5MP1gWvz3Bolow="
     try:
         token_list = token_str.strip().split('.')
         header_body_token = token_list[0] + "." + token_list[1]
@@ -138,7 +130,7 @@ def validate(token_str, secret_key, req_permission):
         validate_body(body_dict)
         validate_signature(header_body_token, secret_key, header_dict, signature_token)
         validate_permission_with_alg(body_dict["Permission"], header_dict["Alg"])
-        validate_permission_requirement(PermissionEnum.ADMIN, body_dict["Permission"])
+        validate_permission_requirement(req_permission, body_dict["Permission"])
 
     except ValueError as err:
         traceback.print_exc()
